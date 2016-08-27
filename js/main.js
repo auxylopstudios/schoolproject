f21e6b27e5ee8d71eff1451339ebc97693ab4543
auxylopstudios <auxylopstudios@gmail.com>
2016-08-27 00:19:53 +0000

    just added some javascript files

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+//image-scale
+$(function() {
+
+
+    /*---------------------------------------------------- */
+    /* Preloader
+     ------------------------------------------------------ */
+
+
+
+        // will first fade out the loading animation
+        $("#loader").fadeOut("slow", function(){
+
+            // will fade out the whole DIV that covers the website.
+            $("#preloader").delay(300).fadeOut("slow");
+
+        });
+
+    
+});
